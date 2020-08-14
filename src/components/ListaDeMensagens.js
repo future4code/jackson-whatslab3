2e0cfc94ceb702da1f0be65d7f0ce2cdf59d590a
Elizabeth-Souza <elizabethbarbosa691@gmail.com>
2020-08-14 15:34:08 -0300

    mensagens

--- src/components/ListaDeMensagens.js
+++ src/components/ListaDeMensagens.js
@@ -1,7 +1,36 @@
 import React, { Component } from 'react'
+import styled from 'styled-components'
 
+
+  
 export class ListaDeMensagens extends Component {
+
   render() {
-    return <p>ListaDeMensagens</p>
+    const List = styled.ul`
+    background-color: red;
+    list-style-type: none;
+   `;
+
+    const Mensagens = [
+      {nome: "Eliz", mensagem: "olá"},
+      {nome: "Breno", mensagem: "hi"}
+    ]
+    return (
+      <List>
+        {Mensagens.map(mensagem =>
+          {return(
+            <div>
+              <strong> {mensagem.nome} </strong>
+              <li> {mensagem.mensagem} </li>
+            </div>
+          )})}
+      </List>
+
+    )
+    
   }
+  
 }
+
+
+
